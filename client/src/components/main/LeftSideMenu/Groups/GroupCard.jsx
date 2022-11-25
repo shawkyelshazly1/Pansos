82f7711ae93306847a4ac766ac1e6ea9d6ae84e1
Shawky Elshazly <Shawkyelshazly2@gmail.com>
2022-11-25 21:02:58 +0200

    finished populating user groups section with joined groups for the user

--- client/src/components/main/LeftSideMenu/Groups/GroupCard.jsx
+++ client/src/components/main/LeftSideMenu/Groups/GroupCard.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import S from "underscore.string";
 
-export default function GroupCard() {
+export default function GroupCard({ group }) {
 	return (
-		<Link to={"/group/123456"}>
+		<Link to={`/group/${group.id}`}>
 			<div className="flex felx-row gap-3 items-center cursor-pointer">
 				<img
-					className="w-10 rounded-lg object-cover"
-					src="https://i.postimg.cc/4yc4NfPr/hacker-team-esport-logo-177315-79-1.webp"
+					className="w-10 h-10 rounded-lg object-cover"
+					src={group.photo.url}
 					alt=""
 				/>
-				<h1 className="text-[#53596d] font-medium">Pickolab Studio</h1>
+				<h1 className="text-[#53596d] font-medium">
+					{S(group.name).titleize().value()}
+				</h1>
 			</div>
 		</Link>
 	);
